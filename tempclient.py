61115da3ae9751adc8d07bc361dab5fa3c7f2008
肖克成 <xiaokecheng@xiaokechengdeMac-mini.local>
2017-03-10 18:45:10 +0800

    now client can work at rate of 1/12 HZ

--- tempclient.py
+++ tempclient.py
@@ -1,24 +1,20 @@
-
 from urllib import request
 import re
 import time
 
 def getTemp():
-	# pass
-	# try:
-	# 	tfile = open("/sys/bus/w1/devices/28-00000494cb79/w1_slave")
-	# except Exception as e:
-	# 	print(e)
-	# else:
-	# 	text = tfile.read()
-	# finally:
-	# 	tfile.close()
 
-	# secondline = text.split("\n")[1]
-	# temperaturedata = secondline.split(" ")[9]
-	# temperature = float(temperaturedata[2:]) / 1000
-	# return temperature
-	return 32
+	try:
+		tfile = open("/sys/bus/w1/devices/28-0316a0fc60ff/w1_slave")
+	except Exception as e:
+		print(e)
+	else:
+		text = tfile.read()
+	finally:
+		tfile.close()
+	temperaturedata = re.findall(' t\=(\S+)$', text)
+	temperature = float(temperaturedata[0]) / 1000
+	return temperature
 
 apikey = '85b0c7f75ce26bdba4b5d16e64dd31d8'
 url = 'http://api.yeelink.net/v1.0/device/355310/sensor/401857/datapoints'
@@ -34,11 +30,6 @@ while True:
 	except Exception as e:
 		print('link error')
 		print(e)
-		break
 	else:
 		print('link succeed')
-	time.sleep(10)
-
-
-
-
+	time.sleep(12)
